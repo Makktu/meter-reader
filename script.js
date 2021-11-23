1992ff5a9fefbda7726e87667a7faf41f9b4a888
Makktu <johnmcna@gmail.com>
2021-11-23 18:03:48 +0000

    some error handling

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 "use strict";
 
+function handleErrors(msg) {
+    messageArea.insertAdjacentText("beforeend", msg);
+    throw new Error(msg);
+}
+
+const messageArea = document.querySelector(".messages");
+
 const okBtn = document.getElementById("ok-btn");
+const latField = document.getElementById("lat");
+const lonField = document.getElementById("lon");
 
 okBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("clicked!");
+    messageArea.textContent = "";
+    e.preventDefault(); // stop form from reloading itself
+    const lat = +latField.value;
+    const lon = +lonField.value; // assign the iunput values to variables and convert to numbers
+    console.log(typeof lat, typeof lon);
+    // ! need to check input and reject if not a GPS pairing, i.e. not numbers
+    if (!lat || !lon) {
+        handleErrors("⚠️ Enter numerical values for both");
+    }
+
+    if (typeof lat !== "number" || !typeof lon !== "number") {
+        if (typeof lat !== "number") latField.value = "";
+        if (typeof lon !== "number") lonField.value = "";
+        handleErrors("⚠️ NUMBERS only for both");
+    }
 });
