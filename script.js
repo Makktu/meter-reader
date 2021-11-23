d0e220fb8d826740f82b5f26b3014a13db7273ce
Makktu <johnmcna@gmail.com>
2021-11-23 19:06:35 +0000

    stuck on api

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 "use strict";
 
+// *******************************************************
+// *******************************************************
+
+const clearFields = (msg) => {
+    latField.value = "";
+    lonField.value = "";
+    messageArea.insertAdjacentText("beforeend", msg);
+};
+
 function handleErrors(msg) {
     messageArea.insertAdjacentText("beforeend", msg);
     throw new Error(msg);
 }
+
+function whereAmI(lat, lon) {
+    // ! first, check that lat and lon are numbers
+    // refine this check to limit it to GPS numbers eventually
+
+    // if (!isNaN(lat) || !isNaN(lon)) {
+    //     clearFields("⚠️ Enter numbers only");
+    //     return;
+    // }
+
+    // ! And so to business:
+    fetch(`http://geocode.xyz/${lat},${lon}?geoit=json`).then((response) => {
+        console.log(response);
+    });
+}
+
+// fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(`${errorMsg} – code ${response.status}`);
+//     }
+//     return response.JSON();
+//   });
+
+// *******************************************************
+// *******************************************************
 
 const messageArea = document.querySelector(".messages");
 
@@ -13,18 +47,12 @@ const lonField = document.getElementById("lon");
 
 okBtn.addEventListener("click", (e) => {
     messageArea.textContent = "";
-    e.preventDefault(); // stop form from reloading itself
-    const lat = +latField.value;
-    const lon = +lonField.value; // assign the iunput values to variables and convert to numbers
-    console.log(typeof lat, typeof lon);
-    // ! need to check input and reject if not a GPS pairing, i.e. not numbers
+    e.preventDefault();
+    const lat = latField.value;
+    const lon = lonField.value;
+    // ! check input exists
     if (!lat || !lon) {
-        handleErrors("⚠️ Enter numerical values for both");
+        handleErrors("⚠️ Enter numbers for both");
     }
-
-    if (typeof lat !== "number" || !typeof lon !== "number") {
-        if (typeof lat !== "number") latField.value = "";
-        if (typeof lon !== "number") lonField.value = "";
-        handleErrors("⚠️ NUMBERS only for both");
-    }
+    whereAmI(parseInt(lat), parseInt(lon));
 });
