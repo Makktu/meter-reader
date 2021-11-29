b001e7b31aea6163cb857072bbcf0c98fc9781b6
Makktu <johnmcna@gmail.com>
2021-11-29 18:24:48 +0000

    First Commit

--- script.js
+++ script.js
@@ -1,58 +1,23 @@
 "use strict";
 
-// *******************************************************
-// *******************************************************
-
-const clearFields = (msg) => {
-    latField.value = "";
-    lonField.value = "";
-    messageArea.insertAdjacentText("beforeend", msg);
-};
-
-function handleErrors(msg) {
-    messageArea.insertAdjacentText("beforeend", msg);
-    throw new Error(msg);
+function formatTimeDate() {
+    let currentTimeDate = new Date();
+    timeDate.textContent = currentTimeDate.toString().slice(0, 21);
 }
-
-function whereAmI(lat, lon) {
-    // ! first, check that lat and lon are numbers
-    // refine this check to limit it to GPS numbers eventually
-
-    // if (!isNaN(lat) || !isNaN(lon)) {
-    //     clearFields("⚠️ Enter numbers only");
-    //     return;
-    // }
-
-    // ! And so to business:
-    fetch(`http://geocode.xyz/${lat},${lon}?geoit=json`).then((response) => {
-        console.log(response);
-    });
-}
-
-// fetch(url).then(response => {
-//     if (!response.ok) {
-//       throw new Error(`${errorMsg} – code ${response.status}`);
-//     }
-//     return response.JSON();
-//   });
-
-// *******************************************************
-// *******************************************************
 
 const messageArea = document.querySelector(".messages");
-
+const timeDate = document.querySelector(".timedatestamp");
 const okBtn = document.getElementById("ok-btn");
-const latField = document.getElementById("lat");
-const lonField = document.getElementById("lon");
+const changeTimeDateBtn = document.querySelector(".change");
+const readingField = document.getElementById("reading");
 
 okBtn.addEventListener("click", (e) => {
-    messageArea.textContent = "";
     e.preventDefault();
-    const lat = latField.value;
-    const lon = lonField.value;
-    // ! check input exists
-    if (!lat || !lon) {
-        handleErrors("⚠️ Enter numbers for both");
-    }
-    whereAmI(parseInt(lat), parseInt(lon));
+    // stuff
 });
+
+changeTimeDateBtn.addEventListener("click", () => {
+    // Need to load a time/date picker and pass result to
+});
+
+formatTimeDate();
